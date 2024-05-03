3157f93f795501044159e60050d9624de14e37fd
Josef Nobach <josef.nobach@outlook.com>
2024-05-03 07:31:03 +0200

    removed code from open webpage

--- out/commands/openWebpage.js
+++ out/commands/openWebpage.js
@@ -25,17 +25,8 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handleOpenWebpage = void 0;
 const vscode = __importStar(require("vscode"));
-// This will store our opened URLs
-let openedUrls = new Set();
 function handleOpenWebpage() {
     const url = 'https://chat.openai.com/'; // Specify your URL here
-    // Check if the URL is already opened
-    if (openedUrls.has(url)) {
-        vscode.window.showInformationMessage('This URL is already opened, attempting to focus...');
-    }
-    else {
-        openedUrls.add(url);
-    }
     // Try to open or focus the URL
     vscode.env.openExternal(vscode.Uri.parse(url));
 }
--- src/commands/openWebpage.ts
+++ src/commands/openWebpage.ts
@@ -1,17 +1,7 @@
 import * as vscode from 'vscode';
 
-// This will store our opened URLs
-let openedUrls = new Set<string>();
-
 export function handleOpenWebpage() {
     const url = 'https://chat.openai.com/'; // Specify your URL here
-
-    // Check if the URL is already opened
-    if (openedUrls.has(url)) {
-        vscode.window.showInformationMessage('This URL is already opened, attempting to focus...');
-    } else {
-        openedUrls.add(url);
-    }
 
     // Try to open or focus the URL
     vscode.env.openExternal(vscode.Uri.parse(url));
